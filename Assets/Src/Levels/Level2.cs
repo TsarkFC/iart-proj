98949c60a67ce2fb2f4d47b034823153802dc61c
TsarkFC <up201806779@fe.up.pt>
2021-03-17 23:19:40 +0000

    Level structure implemented

---
+++ Assets/Src/Levels/Level2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2 : Level
+{
+    void Start()
+    {
+        base.piecePrefabDict = new Dictionary<PieceType, GameObject>();
+        base.board = new PieceType[,] {{PieceType.BARRIER, PieceType.PIECE_ORANGE, PieceType.BARRIER, PieceType.TARGET_RED},
+                                    {PieceType.BARRIER, PieceType.PIECE_RED, PieceType.TARGET_ORANGE, PieceType.EMPTY},
+                                    {PieceType.BARRIER, PieceType.EMPTY, PieceType.EMPTY, PieceType.BARRIER},
+                                    {PieceType.EMPTY, PieceType.BARRIER, PieceType.BARRIER, PieceType.EMPTY}};
+        base.buildBoard();
+    }
+
+    void Update()
+    {
+        
+    }
+}
