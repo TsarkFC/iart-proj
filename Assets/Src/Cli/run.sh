5995e83a1329c8cf39bec6c4c9f743bb1e2eb5c5
TsarkFC <up201806779@fe.up.pt>
2021-03-23 22:46:20 +0000

    cli

---
+++ Assets/Src/Cli/run.sh
@@ -0,0 +1,2 @@
+mcs -out:Logic.exe Logic.cs ../GameLogic/Piece.cs ../GameLogic/Position.cs ../GameLogic/PieceOrderer.cs
+mono Logic.exe
--- Assets/Src/Terminal/run.sh
+++
@@ -1,2 +0,0 @@
-mcs -out:Logic.exe Logic.cs Piece.cs Position.cs PieceOrderer.cs
-mono Logic.exe
