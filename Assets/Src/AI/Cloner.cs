506534aee203c0a5fcb2541444879f160b105524
rafaavc <rafavc2508@gmail.com>
2021-04-03 16:50:52 +0100

    Added reference to cloner

--- Assets/Src/AI/Cloner.cs
+++ Assets/Src/AI/Cloner.cs
@@ -3,6 +3,7 @@ using System.IO;
 
 namespace cloner
 {
+    // https://stackoverflow.com/questions/129389/how-do-you-do-a-deep-copy-of-an-object-in-net
     public static class Cloner
     {
         public static T DeepClone<T>(this T obj)
